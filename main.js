daf16a276ced336f1673c6e226af588ca91f8b22
YukiKubota <rakudalms@gmail.com>
2022-06-30 14:05:32 +0900

    Add form to Add new Todos

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 console.log("Hello World")
+
+const inputTodo = document.getElementsByClassName('inputTodo')[0];
+const addBtn = document.getElementsByClassName('addBtn')[0];
+const todoList = document.getElementsByClassName('todoList')[0];
+
+// TODO追加処理
+const addTodos = (todo) => {
+  if(todo){
+    console.log(todo)
+    const listElement = document.createElement('li');
+    const listItem = todoList.appendChild(listElement);
+    listItem.innerHTML = todo;
+
+    const doneBtn = document.createElement('span');
+    doneBtn.className = 'doneBtn';
+    doneBtn.innerHTML = 'Done';
+    listElement.appendChild(doneBtn)
+
+    const deleteBtn = document.createElement('span');
+    deleteBtn.className = 'deleteBtn';
+    deleteBtn.innerHTML = 'Delete';
+    listElement.appendChild(deleteBtn)
+  }
+};
+addBtn.addEventListener('click', e => {
+  e.preventDefault();
+  const todo = inputTodo.value;
+  addTodos(todo);
+  inputTodo.value = '';
+});
