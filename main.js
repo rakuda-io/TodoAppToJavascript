3dfa02108ef1412dc474480882896679500bedad
YukiKubota <rakudalms@gmail.com>
2022-07-22 17:53:11 +0900

    Filter the todo list with unfinished tasks on top, finished on bottom

--- main.js
+++ main.js
@@ -1,107 +1,145 @@
-console.log("Hello World");
-
-const inputTodo = document.getElementsByClassName('inputTodo')[0];
-const addBtn = document.getElementsByClassName('addBtn')[0];
+const inputArea = document.getElementsByClassName('inputArea')[0];
+const submitBtn = document.getElementsByClassName('submitBtn')[0];
 const todoList = document.getElementsByClassName('todoList')[0];
 const totalCount = document.getElementsByClassName('totalCount')[0];
 const doneCount = document.getElementsByClassName('doneCount')[0];
-let data = [];
-let id = 0;
-// let status = 'WIP';
+let data = [{id: 0, title: 'Javascriptの勉強', status: 'wip'}, {id: 1, title: '子供の寝かしつけ', status: 'done'}];
+let id = 2;
 
 // ページ読み込み時の処理
-window.addEventListener('DOMContentLoaded', e => {
-  e.preventDefault();
+window.addEventListener('DOMContentLoaded', event => {
+  event.preventDefault();
   data.forEach(todo => {
-    displayTodo(todo.title);
+    buildTodo(todo.title);
   });
+  console.log("Hello World");
 });
 
-// TODOのリスト表示する関数
-const displayTodo = (todo) => {
+// TODOのリスト表示
+const buildTodo = (todo) => {
+  // data配列から該当データを取得
+  let matchedData = data.find(v => v.title == todo);
+
   // DOM生成
   let listElement = document.createElement('li');
   let listItem = todoList.appendChild(listElement);
   listItem.innerHTML = todo;
-  let ul = listElement.parentNode;
-  totalCount.innerHTML = ul.childElementCount;
 
   const doneBtn = document.createElement('span');
   doneBtn.className = 'doneBtn';
   doneBtn.innerHTML = 'Done';
-  doneBtn.setAttribute('id', `${id}`);
+  doneBtn.setAttribute('id', `${matchedData.id}`);
+  listElement.className = matchedData.status;
   listElement.appendChild(doneBtn);
 
   const deleteBtn = document.createElement('span');
   deleteBtn.className = 'deleteBtn';
   deleteBtn.innerHTML = 'Delete';
-  deleteBtn.setAttribute('id', `${id}`);
-  id += 1;
+  deleteBtn.setAttribute('id', `${matchedData.id}`);
   listElement.appendChild(deleteBtn);
 
+  doneFuction(doneBtn);
+  deleteFunction(deleteBtn);
+  updateCounter();
+};
+
+// TODO追加機能
+submitBtn.addEventListener('click', event => {
+  event.preventDefault();
+  let inputValue = inputArea.value;
+  // バリデーション
+  if(inputValue && !(data.some(v => v.title === inputValue))){
+    // data配列に追加
+    addData(inputValue);
+    orderByStatus();
+    rebuildTodoList();
+  } else {
+    console.log('[Error] 同じ名前は登録出来ません')
+  }
+  // 入力欄を空に
+  inputArea.value = '';
+});
+
+// TODO完了機能
+const doneFuction = (doneBtn) => {
   // DoneBtnを監視
-  doneBtn.addEventListener('click', e => {
-    e.preventDefault();
+  doneBtn.addEventListener('click', event => {
+    event.preventDefault();
     let list = e.target.parentNode;
     list.classList.toggle('done');
-    doneTodo(doneBtn);
-    // カウント数を更新
-    doneCount.innerHTML = document.getElementsByClassName('done').length;
+    list.classList.toggle('wip');
+    toggleStatus(doneBtn);
   });
 
-  // DeleteBtnを監視
-  deleteBtn.addEventListener('click', e => {
-    e.preventDefault();
-    deleteTodo(deleteBtn);
-    // カウント数を更新
-    ul = document.getElementsByClassName('todoList')[0];
-    totalCount.innerHTML = ul.childElementCount;
-    doneCount.innerHTML = document.getElementsByClassName('done').length;
-  });
+  // ステータスを切り替え
+  const toggleStatus = (doneBtn) => {
+    let matchedData = data.find(v => v.id == doneBtn.getAttribute('id'));
 
-  // TODOのステータスをトグルする関数
-  const doneTodo = (doneBtn) => {
-    let matchedData = data.find(value => value.id == doneBtn.getAttribute('id'));
     if(doneBtn.parentNode.className == 'done') {
-      matchedData.status = 'DONE';
-      console.log(`[Changed to DONE] ${todo}`);
+      matchedData.status = 'done';
+      console.log(`[Changed to DONE] ${matchedData.title}`);
     } else {
-      matchedData.status = 'WIP';
-      console.log(`[Changed to WIP] ${todo}`);
+      matchedData.status = 'wip';
+      console.log(`[Changed to WIP] ${matchedData.title}`);
     };
-    console.log(data);
-  };
 
-  // TODOを削除する関数
-  const deleteTodo = (deleteBtn) => {
-    // DOMを削除
-    let deleteDom = deleteBtn.closest('li');
-    todoList.removeChild(deleteDom);
-    // data配列から削除
-    data = data.filter(i => i.id.toString() !== deleteBtn.getAttribute('id'));
-    console.log(`[Deleted] ${todo}`);
+    // Statusに応じて並べ替えしてリストを再描画
+    orderByStatus();
+    rebuildTodoList();
+    updateCounter();
     console.log(data);
   };
 };
 
-// TODO追加
-addBtn.addEventListener('click', e => {
-  e.preventDefault();
-  let inputValue = inputTodo.value;
+// TODO削除機能
+const deleteFunction = (deleteBtn) => {
+  // DeleteBtnを監視
+  deleteBtn.addEventListener('click', event => {
+    event.preventDefault();
+    deleteTodo(deleteBtn);
+    updateCounter();
+  });
 
-  if(inputValue && !(data.some(d => d.title === inputValue))){
-    addTodo(inputValue);
-    displayTodo(data.slice(-1)[0].title);
-  } else {
-    console.log('[error] 同じ名前は登録出来ません')
-  }
-  inputTodo.value = '';
-});
+  // TODOを削除する関数
+  const deleteTodo = (deleteBtn) => {
+    console.log(`[Deleted] ${(data.find(v => v.id == deleteBtn.getAttribute('id'))).title}`);
+    // DOMを削除
+    let deleteDom = deleteBtn.closest('li');
+    todoList.removeChild(deleteDom);
+    // data配列から削除
+    data = data.filter(v => v.id.toString() !== deleteBtn.getAttribute('id'));
+    console.log(data);
+  };
+};
 
-// TODOを追加する関数
-const addTodo = (inputValue) => {
-  let initStatus = 'WIP';
-  data.push({id: id, title: inputValue, status: initStatus});
+// dataに新しいTODOを追加する関数
+const addData = (inputValue) => {
+  data.push({id: id, title: inputValue, status: 'wip'});
+  id += 1;
   console.log(`[Added] ${inputValue}`);
   console.log(data);
+};
+
+// TODOリストのDOMを再構築する関数
+const rebuildTodoList = () => {
+  // リストを初期化
+  while( todoList.firstChild ){
+    todoList.removeChild( todoList.firstChild );
+  };
+  // リストを再描画
+  data.forEach(todo => {
+    buildTodo(todo.title);
+  });
+};
+
+// Statusで並べ替えする関数
+const orderByStatus = () => {
+  data.sort((a,b) => a.id - b.id);
+  data.sort((a,b) => a.status.length-b.status.length);
+};
+
+// カウント数を更新する関数
+const updateCounter = () => {
+  totalCount.innerHTML = data.length;
+  doneCount.innerHTML = data.filter(value => value.status == 'done').length;
 };
